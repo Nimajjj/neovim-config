bd1901c4e59ad9a1293915c963118352f33e8179
Nimajjj <benjamin.borello@aol.com>
2023-12-14 20:55:56 +0100

    format keybinding script

--- lua/keybinding.lua
+++ lua/keybinding.lua
@@ -5,7 +5,6 @@ local global = vim.g
 -- Change leader key to <Space>
 global.leader = " "
 
-
 -- Map function
 function map(mode, lhs, rhs, opts)
     local options = { noremap = true }
@@ -15,20 +14,11 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-
--- Hop
-map("n", "<Leader><Leader>", ":HopWord<CR>", {silent = true})
-
--- Toggleterm
-map("t", "<ESC>", "<c-\\><c-n>", {silent = true})
-
-
 -- Use Ctrl-[hjkl] to select the active split
 map("n", "<C-k>", ":wincmd k<CR>", {silent = true})
 map("n", "<C-j>", ":wincmd j<CR>", {silent = true})
 map("n", "<C-h>", ":wincmd h<CR>", {silent = true})
 map("n", "<C-l>", ":wincmd l<CR>", {silent = true})
-
 
 -- Use Ctrl-[arrows] to resize active pane 
 map("n", "<C-up>", ":horizontal resize -1<CR>", {silent = true})
@@ -36,14 +26,20 @@ map("n", "<C-down>", ":horizontal resize +1<CR>", {silent = true})
 map("n", "<C-left>", ":vertical resize -1<CR>", {silent = true})
 map("n", "<C-right>", ":vertical resize +1<CR>", {silent = true})
 
-
 -- Disable arrow for navigation in normal mode
 map("n", "<up>", "<Nop>", {silent = true})
 map("n", "<down>", "<Nop>", {silent = true})
 map("n", "<left>", "<Nop>", {silent = true})
 map("n", "<right>", "<Nop>", {silent = true})
 
-
 -- Telescope
 map("n", "<leader>lg", ":Telescope live_grep<CR>", {silent = false})
+
+-- Neotree
 map("n", "<leader>f", ":NeoTreeFocusToggle<CR>", {silent = false})
+
+-- Hop
+map("n", "<Leader><Leader>", ":HopWord<CR>", {silent = true})
+
+-- Toggleterm
+map("t", "<ESC>", "<c-\\><c-n>", {silent = true})
